5437f31cd39e74478f3d18a0a81d36c56ec7b3f4
anvaka <anvaka@gmail.com>
2021-10-13 21:54:09 -0700

    Better names, fixed spelling

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
-var thetamin = 0,
-  thetamax = 6 * Math.PI,
-  period = 5,
-  linespacing = 1 / 30,
-  linelength = linespacing / 2,
-  yscreenoffset = 300,
-  xscreenoffset = 260,
-  xscreenscale = 360,
-  yscreenscale = 360,
-  ycamera = 2,
-  zcamera = -3,
+var thetaMin = 0;
+var thetaMax = 6 * Math.PI;
+var period = 5;
+var lineSpacing = 1 / 30;
+var lineLength = lineSpacing / 2;
+var yScreenOffset = 300;
+var xScreenOffset = 260;
+var xScreenScale = 360;
+var yScreenScale = 360;
+var yCamera = 2;
+var zCamera = -3;
 
-  rate = 1 / (2 * Math.PI), // every rotation y gets one bigger
-  factor = rate / 3;
+var rate = 1 / (2 * Math.PI); // every rotation y gets one bigger
+var factor = rate / 3;
 
 function run() {
   var ctx = document.getElementById('scene').getContext('2d'),
       spirals = [
         new Spiral({
           foreground: "#220000", // Second shadow for red spiral
-          angleoffset: Math.PI * 0.92,
+          angleOffset: Math.PI * 0.92,
           factor: 0.90 * factor
         }),
         new Spiral({
           foreground: "#002211", // Second shadow for cyan spiral
-          angleoffset: -Math.PI * 0.08,
+          angleOffset: -Math.PI * 0.08,
           factor: 0.90 * factor
         }),
         new Spiral({
           foreground: "#660000", // red spiral shadow
-          angleoffset: Math.PI * 0.95,
+          angleOffset: Math.PI * 0.95,
           factor: 0.93 * factor
         }),
         new Spiral({
           foreground: "#003322", // cyan spiral shadow
-          angleoffset: -Math.PI * 0.05,
+          angleOffset: -Math.PI * 0.05,
           factor: 0.93 * factor
         }),
         new Spiral({
           foreground: "#ff0000", // red Spiral
-          angleoffset: Math.PI,
+          angleOffset: Math.PI,
           factor: factor
         }),
         new Spiral({
           foreground: "#00ffcc", // cyan spiral
-          angleoffset: 0,
+          angleOffset: 0,
           factor: factor
         })];
 
@@ -83,20 +83,24 @@ function run() {
     function computeLineSegments() {
       var lineSegments = {};
       var factor = config.factor;
-      var thetanew, thetaold;
+      var thetaNew, thetaOld;
       for (var offset = 0; offset > -period; offset--) {
         lineSegments[offset] = lines = [];
-        for (var theta = thetamin + getdtheta(thetamin, offset * linespacing / period, rate, factor); theta < thetamax; theta += getdtheta(theta, linespacing, rate, factor)) {
-          thetaold = (theta >= thetamin) ? theta : thetamin;
-          thetanew = theta + getdtheta(theta, linelength, rate, factor);
+        for (
+          var theta = thetaMin + getThetaChangeRate(thetaMin, offset * lineSpacing / period, rate, factor); 
+          theta < thetaMax; 
+          theta += getThetaChangeRate(theta, lineSpacing, rate, factor)
+        ) {
+          thetaOld = (theta >= thetaMin) ? theta : thetaMin;
+          thetaNew = theta + getThetaChangeRate(theta, lineLength, rate, factor);
 
-          if (thetanew <= thetamin) {
+          if (thetaNew <= thetaMin) {
             continue;
           }
 
           lines.push({
-            start: getPointByAngle(thetaold, factor, config.angleoffset, rate),
-            end: getPointByAngle(thetanew, factor, config.angleoffset, rate)
+            start: getPointByAngle(thetaOld, factor, config.angleOffset, rate),
+            end: getPointByAngle(thetaNew, factor, config.angleOffset, rate)
           });
         }
       }
@@ -113,11 +117,11 @@ function run() {
     ctx.beginPath();
   }
 
-  function getPointByAngle(theta, factor, angleoffset, rate) {
-    var x = theta * factor *  Math.cos(theta + angleoffset);
-    var z = - theta * factor * Math.sin(theta + angleoffset);
+  function getPointByAngle(theta, factor, angleOffset, rate) {
+    var x = theta * factor *  Math.cos(theta + angleOffset);
+    var z = - theta * factor * Math.sin(theta + angleOffset);
     var y = rate * theta;
-    // now that we have 3d coorinates, project them into 2d space:
+    // now that we have 3d coordinates, project them into 2d space:
     var point = projectTo2d(x, y, z);
     // calculate point's color alpha level:
     point.alpha = Math.atan((y * factor / rate * 0.1 + 0.02 - z) * 40) * 0.35 + 0.65;
@@ -125,14 +129,14 @@ function run() {
     return point;
   }
 
-  function getdtheta(theta, lineLength, rate, factor) {
+  function getThetaChangeRate(theta, lineLength, rate, factor) {
     return lineLength / Math.sqrt(rate * rate + factor * factor * theta * theta);
   }
 
   function projectTo2d(x, y, z) {
     return {
-      x: xscreenoffset + xscreenscale * (x / (z - zcamera)),
-      y: yscreenoffset + yscreenscale * ((y - ycamera) / (z - zcamera))
+      x: xScreenOffset + xScreenScale * (x / (z - zCamera)),
+      y: yScreenOffset + yScreenScale * ((y - yCamera) / (z - zCamera))
     };
   }
 
